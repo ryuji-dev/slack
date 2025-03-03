6b8438e377f9c95f1cfd38c7b17d2ec204b0a257
Ryuji <hi_ryuji@naver.com>
2025-03-04 00:48:58 +0900

    feat: ConfigModule 사용하기

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly configService: ConfigService) {}
+
   getHello(): string {
-    return 'Hello World!';
+    return this.configService.get('WORD') || 'default value';
+    // process.env.WORD 대신 nest가 제공하는 configService를 사용 -> 환경변수 관리 용이(의존성 주입)
   }
 }
