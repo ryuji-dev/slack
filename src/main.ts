354073179efa004e00914a3970605c79fdec3d61
Ryuji <hi_ryuji@naver.com>
2025-03-04 00:36:57 +0900

    feat: Hot reload 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
-  console.log(`Server is running on port ${process.env.PORT ?? 3000}`);
+  console.log(
+    `Server is running on port http://localhost:${process.env.PORT ?? 3000}`,
+  );
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap().catch((error) => {
   console.error(error);
